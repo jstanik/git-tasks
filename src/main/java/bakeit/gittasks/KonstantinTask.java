a26cb10ff394209c89cec4334abd0e9030897d43
jstanik <juraj.stanik@gmail.com>
2024-03-11 09:01:14 +0100

    Create tasks for each member of the club (#1)

---
+++ src/main/java/bakeit/gittasks/KonstantinTask.java
@@ -0,0 +1,18 @@
+package bakeit.gittasks;
+
+public class KonstantinTask {
+
+  /**
+   * Find the greatest common divisor (GCD) of the integers {@code a} and {@code b}.
+   * <p>
+   * In mathematics, the greatest common divisor (GCD) of two or more integers, which are not all
+   * zero, is the largest positive integer that divides each of the integers.
+   * </p>
+   *
+   * @return the greatest common divisor of the two integers.
+   */
+  public int greatestCommonDivisor(int a, int b) {
+    throw new UnsupportedOperationException();
+  }
+
+}
