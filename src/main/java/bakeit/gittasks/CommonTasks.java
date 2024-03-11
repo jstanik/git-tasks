78bcd574775268103f866b6b220c85090f5bc9ea
Juraj Staník <juraj.stanik@gmail.com>
2024-03-11 08:58:38 +0100

    Create tasks for each member of the club

---
+++ src/main/java/bakeit/gittasks/CommonTasks.java
@@ -0,0 +1,40 @@
+package bakeit.gittasks;
+
+public class CommonTasks {
+  
+  /**
+   * Task for Maxim.
+   *
+   * @return the maximum value of the two given numbers.
+   */
+  public int findMax(int a, int b) {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Task for Andriy
+   *
+   * @return the minimum value of the two given numbers.
+   */
+  public int findMin(int a, int b) {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Task for David
+   *
+   * @return {@code true} if the {@code value} is "odd"; {@code false} otherwise.
+   */
+  public boolean isOdd(int value) {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Task for Konstantin.
+   *
+   * @return {@code true} if the {@code value} is "even"; {@code false} otherwise.
+   */
+  public boolean isEven(int value) {
+    throw new UnsupportedOperationException();
+  }
+}
