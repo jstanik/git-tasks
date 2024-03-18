823232a56ad2ab0c9818f6ffb56dc84ac638fd51
Kruska <tandgirokamado7@gmail.com>
2024-03-18 15:27:41 +0100

    I create program

--- src/main/java/bakeit/gittasks/AndriyTask.java
+++ src/main/java/bakeit/gittasks/AndriyTask.java
@@ -2,17 +2,28 @@ package bakeit.gittasks;
 
 public class AndriyTask {
 
-  /**
-   * Find the greatest common divisor (GCD) of the integers {@code a} and {@code b}.
-   * <p>
-   * In mathematics, the greatest common divisor (GCD) of two or more integers, which are not all
-   * zero, is the largest positive integer that divides each of the integers.
-   * </p>
-   *
-   * @return the greatest common divisor of the two integers.
-   */
-  public int greatestCommonDivisor(int a, int b) {
-    throw new UnsupportedOperationException();
+  public static int GreatestCommonDivisor(int a, int b) {
+    // Ensure that b is not greater than a
+    if (b > a) {
+      int temp = a;
+      a = b;
+      b = temp;
+    }
+
+    // Apply the Euclidean algorithm
+    while (b != 0) {
+      int temp = b;
+      b = a % b;
+      a = temp;
+    }
+
+    return a;
   }
 
+  public static void main(String[] args) {
+    int a = 1902;
+    int b = 33;
+    int gcd = GreatestCommonDivisor(a, b);
+    System.out.println("GCD of " + a + " and " + b + " is: " + gcd);
+  }
 }
